dbb611c2423736448131e4a730f90dc5a0159c69
kavabasboga <kawabasboga789@gmail.com>
2026-01-06 21:39:46 +0300

    kavabasboga

--- datacleaning.py
+++ datacleaning.py
@@ -1,1 +1,13 @@
+import pandas as pd
+def clean_weather_data(df):
+    if df.empty:
+        print("No data to clean.")
+        return df
 
+    df["temperature"] = pd.to_numeric(df["temperature"], errors="coerce")
+    df.dropna(inplace=True)
+
+    df["date"] = pd.to_datetime(df["date"], errors="coerce")
+    df.dropna(inplace=True)
+
+    return df
